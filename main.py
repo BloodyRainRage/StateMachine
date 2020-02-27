41c1992a3a41978317e32031abfbaec4f398c632
BloodyRainRage <bloodyrainrage@gmail.com>
2020-02-27 12:56:15 +0300

    Update main.py

--- main.py
+++ main.py
@@ -6,12 +6,7 @@ import collections
 st = StateMachine.StateMachine()
 
 dic = ['0', '1', '0', '2', '0', '2']
-# dic.append('0')
-# dic.append('1')
-# dic.append('0')
-# dic.append('2')
-# dic.append('0')
-# dic.append('2')
+
 print("Dictionary:", ''.join(dic))
 #print(''.join(dic))
 
